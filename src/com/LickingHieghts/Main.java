b2ce1d54163eb7fd0b032b8ab8261f0935c4c0c8
Wilbert Del Rosario <wilbert.delrosario.2022@student.lhschools.org>
2018-11-08 07:38:30 -0500

    added some stuff

--- src/com/LickingHieghts/Main.java
+++ src/com/LickingHieghts/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
 
-        System.out.println("hi");
+        System.out.println("Welcome To the Hang Guy game");
 
 
 
