36ba7af1ca81c5106ae31533b7732d7748a96744
Wilbert Del Rosario <wilbert.delrosario.2022@student.lhschools.org>
2018-11-08 08:37:24 -0500

    Made System.out that explains the main parts of the game

--- src/com/LickingHieghts/Main.java
+++ src/com/LickingHieghts/Main.java
@@ -6,6 +6,12 @@ public class Main {
 	// write your code here
 
         System.out.println("Welcome To the Hang Guy game");
+        System.out.println("Rules:this - represents a letter for every - their is one letter");
+        System.out.println("Every time you guess the wrong letter the hang guy will be built ");
+        System.out.println("Once you guess the letter correctly one of spaces will be filled in with that letter");
+        System.out.println("Good Luck!");
+
+
 
 
 
